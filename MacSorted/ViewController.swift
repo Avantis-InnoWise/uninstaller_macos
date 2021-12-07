21918c2d46caadcc858e6aa981390215656ce050
Alexander Usnarsky <alexander.usnarski@orangesoft.co>
2021-12-07 15:17:00 +0300

    Create and setup project.

---
+++ MacSorted/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MacSorted
+//
+//  Created by Александр Уснарский on 7.12.21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
